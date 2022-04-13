b91f31021549ba8a1963b11fac7a3f8e4020dc53
Seungjin <infinissible@gmail.com>
2022-04-13 01:31:56 -0700

    created HTML file

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const fs = require('fs');
 
-    console.log('===========');
+const generatePage = require('./src/page-template.js');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
+const profileDataArgs = process.argv.slice(2);
 
+const [name, github] = profileDataArgs;
 
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
